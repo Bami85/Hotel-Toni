3b87771cbfbf28dacc1444b4cbf8d1f85159648a
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-06-27 14:18:34 +0200

    Instagram Kontakt

--- components/Instagram.jsx
+++ components/Instagram.jsx
@@ -12,7 +12,7 @@ const Instagram = () => {
   return (
     <div className='max-w-[1240px] mx-auto text-center py-24'>
         <p className='text-2xl font-bold'>Na ndiq ne Instagram</p>
-        <p className='pb-4'>@Toni</p>
+        <p className='pb-4'>@Hotel Toni</p>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 p-4'>
             <InstagramImg socialImg={Logo} />
             <InstagramImg socialImg={Room} />
