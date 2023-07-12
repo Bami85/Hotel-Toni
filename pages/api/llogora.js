99bd1f19cf49cf31cb7a7770a07b0db77aa20d76
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-07-12 22:29:30 +0200

    mongodb connections

---
+++ pages/api/llogora.js
@@ -0,0 +1,18 @@
+
+import clientPromise from "../../lib/mongodb";
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db("flight");
+  switch (req.method) {
+    case "POST":
+      let bodyObject = JSON.parse(req.body);
+      let newPost = await db.collection("Malaga").insertOne(bodyObject); // Inserimento di un singolo documento
+      res.json(newPost.ops[0]);
+      break;
+    case "GET":
+      const posts = await db.collection("Malaga").find({}).toArray();
+      res.json({ status: 200, data: posts });
+      break;
+  }
+}
