4a31725802b299e26028e6bb517611f275231d5b
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-07-13 00:26:15 +0200

    mongodb connections

--- components/Offers.jsx
+++ components/Offers.jsx
@@ -81,3 +81,65 @@ export default function Dhomat() {
     </div>
   )
 }
+
+
+
+// import { useEffect, useState } from 'react';
+
+// export default function Dhomat() {
+//   const [rooms, setRooms] = useState([]);
+
+//   const fetchDataHandler = async (url, state) => {
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     state(data.data);
+//   };
+
+//   useEffect(() => {
+//     fetchDataHandler('/api/rooms', setRooms);
+//   }, []);
+
+//   console.log(rooms);
+
+//   const reservo = (personName) => {
+//     // Make a reservation in MongoDB and update the number of rooms
+//     // ... Your reservation logic goes here ...
+//     setReservation(`Reservation for ${personName} created successfully!`);
+//   };
+
+//   return (
+//     <div className="bg-white py-24 sm:py-32">
+//       <div className="mx-auto max-w-7xl px-6 lg:px-8">
+//         <ul
+//           role="list"
+//           className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+//         >
+//           {rooms &&
+//             rooms.map((room) => (
+//               <li key={room._id}>
+//                 {/* Render room details */}
+//                 <img
+//                   className="aspect-[3/2] w-full rounded-2xl object-cover"
+//                   src={room.imageUrl}
+//                   alt=""
+//                 />
+//                 <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">
+//                   {room.guestName}
+//                 </h3>
+//                 <p className="text-base leading-7 text-gray-600">
+//                   {room.checkInDate}
+//                 </p>
+//                 <button
+//                   type="button"
+//                   className="rounded bg-black/10 px-2 py-1 text-sm font-semibold text-black shadow-sm hover:bg-white/20"
+//                   onClick={() => reservo(room.price)}
+//                 >
+//                   Rezervo
+//                 </button>
+//               </li>
+//             ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
--- pages/api/rooms.js
+++ pages/api/rooms.js
@@ -3,15 +3,15 @@ import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
-  const db = client.db("flight");
+  const db = client.db("Hotel");
   switch (req.method) {
     case "POST":
       let bodyObject = JSON.parse(req.body);
-      let newPost = await db.collection("Malaga").insertOne(bodyObject); // Inserimento di un singolo documento
+      let newPost = await db.collection("Rooms").insertOne(bodyObject); // Inserimento di un singolo documento
       res.json(newPost.ops[0]);
       break;
     case "GET":
-      const posts = await db.collection("Malaga").find({}).toArray();
+      const posts = await db.collection("Rooms").find({}).toArray();
       res.json({ status: 200, data: posts });
       break;
   }
