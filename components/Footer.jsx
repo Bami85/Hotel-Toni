8d04fa8680b4c1676bcea3f7a49c5acbc85684ac
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-08-31 09:48:04 +0200

    some changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -152,7 +152,7 @@ const navigation = {
             </div>
           </div>
           <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
-            <p className="text-xs leading-5 text-gray-400">&copy; 2023 Hotel Toni, Inc. All rights reserved.</p>
+            <p className="text-xs leading-5 text-gray-400">&copy; 2023 Hotel Toni, Inc.. All rights reserved.</p>
           </div>
         </div>
       </footer>
