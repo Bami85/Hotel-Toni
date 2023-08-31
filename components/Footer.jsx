490f6ae4fa3f5a8592db1aa86fb9db4f6a93ad27
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-08-31 09:52:07 +0200

    new futers

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -153,6 +153,7 @@ const navigation = {
           </div>
           <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
             <p className="text-xs leading-5 text-gray-400">&copy; 2023 Hotel Toni, Inc.. All rights reserved.</p>
+            <p> I wona put some more icons here</p>
           </div>
         </div>
       </footer>
