68564ef325d5856fab491e0d09225a0037b2af97
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-09-06 13:02:45 +0200

    hero message

---
+++ pages/votat.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Portfolio from '../components/Portfolio'
+import Hero from '../components/Hero'
+
+
+
+
+export default function Votat() {
+  return (
+    <div> 
+        <Hero/>
+        <Portfolio heading='Restaurant comming soon' message='Kto jan disa nga ushqimet qe mund te porosisni ne restorant Noeli' />
+     
+    </div>
+  )
+}
