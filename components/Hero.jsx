c4dcd97eda674998e62b5128fbdfad845d4b48ce
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-09-11 23:37:13 +0200

    changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-const Hero = ({ heading, message }) => {
+const Hero = ({ heading, message , description}) => {
   const handleWhatsAppBooking = () => {
     const phoneNumber = '046765876515';
     const whatsappMessage = 'Dua te rezervoj ne Hotel Toni'; // Replace with your desired message
@@ -16,21 +16,26 @@ const Hero = ({ heading, message }) => {
     <>
       <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-video'>
         {/* Overlay */}
-        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[2]' />
         <div className='p-5 text-white z-[2] mt-[-10rem]'>
-          <h2 className='text-5xl font-bold'>{heading}</h2>
-          <p className='py-5 text-xl'>{message} </p>
-          <Link href="/reservationForm">
+          <p className='text-4xl font-bold tracking-tight text-white sm:text-6xl text-center'>{heading} </p>
+          <h2 className='mx-auto max-w-2xl text-center font-bold  '>{message}</h2>
+        
+          <p className="mt-6 text-lg leading-8 text-gray-300"> {description}</p>
+          {/* <Link href="/reservationForm">
             <button className='px-8 py-2 border mr-4'>Prenoto tani Online</button>
           </Link>
           <button className='px-8 py-2 bg-green-500 text-white border rounded' onClick={handleWhatsAppBooking}>
             Rezervo në WhatsApp
-          </button>
+          </button> */}
         </div>
 
         <div className='p-5 text-white z-[2] mt-[-10rem]'></div>
       </div>
+      
+   
     </>
+    
   );
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,16 +5,23 @@ import Hero from '../components/Hero'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Hotel Toni Llogora</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Hero
-        heading="MIRESEVINI"
-        message="Pushimet më të mira fillojnë ne Hotel Toni, aty ku mali perqafon detin."
-      />
-    </div>
+    <>
+      <div>
+        <Head>
+          <title>Hotel Toni Llogora</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div></div>
+        <Hero
+          heading="MIRËSEVINI TE HOTEL TONI "
+          message="Lart mbi det.. Poshte nen qiell.  "
+          description ="Jeni i ftuar të përjetoni dhe të shijoni momente të pazakonta në mes te natyres, aty ku mysafirët ndihen si në shtëpi në dhomat ne mes te pishave te Llogorase."
+          
+        />
+          
+   
+      </div>
+    </>
   )
 }
