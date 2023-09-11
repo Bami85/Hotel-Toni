c75abd7efd0884a3fe3470c97d646aa725e5b707
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-09-12 00:03:21 +0200

    small changes

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -31,9 +31,8 @@ export default function Votat() {
     <>
     {/* /tailwindcss */}
     <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Bookning review</h2>
+          <h2 className="mx-auto max-w-2xl text-center font-bold  ">Komente nga bokning</h2>
         </div>
         <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {votes.map((vote) => (
@@ -68,7 +67,7 @@ export default function Votat() {
           ))}
         </div>
       </div>
-    </div>
+  
     </>
   );
 }
