b5cdd06457d8134922b62d185387f32332ee4969
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-09-12 00:03:39 +0200

    vote

--- pages/rooms.jsx
+++ pages/rooms.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import Portfolio from '../components/Portfolio';
 import Offers from '../components/Offers'
 
 const Rooms = () => {
@@ -9,7 +8,7 @@ const Rooms = () => {
         <Hero heading='Rooms comming soon' message='Kto jan disa nga dhomat qe mund te reservoni ne Hotell Toni' />
          
         <Offers/>
-        <Portfolio/>
+
       
     </div>
   )
