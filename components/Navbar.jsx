1ddaebe2e81e54e938aa911291e484adbf998a44
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-09-14 23:13:20 +0200

    bannerOpen

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,110 +1,197 @@
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import Link from 'next/link'
+import React, { useState, useEffect } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { XMarkIcon } from '@heroicons/react/20/solid'
+
+function Banner({ text, description }) {
+  const [banner, setBanner] = useState(false)
+
+  function closeBanner() {
+    setBanner(true)
+  }
+
+  return (
+    <>
+      {/* <div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+        <div
+          className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+            style={{
+              clipPath:
+                'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+            }}
+          />
+        </div>
+        <div
+          className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
+            style={{
+              clipPath:
+                'polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)',
+            }}
+          />
+        </div>
+        <p className="text-sm leading-6 text-gray-900">
+          <a href="#">
+            <strong className="font-semibold">{description}</strong>
+            <svg
+              viewBox="0 0 2 2"
+              className="mx-2 inline h-0.5 w-0.5 fill-current"
+              aria-hidden="true"
+            >
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            {text}&nbsp;<span aria-hidden="true">&rarr;</span>
+          </a>
+        </p>
+        <div className="flex flex-1 justify-end">
+          <button
+            onClick={closeBanner}
+            type="button"
+            className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
+          >
+            <span className={banner ? 'close' : ''}></span>
+            <XMarkIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
+          </button>
+        </div>
+      </div> */}
+    </>
+  )
+}
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-  const [color, setColor] = useState('transparent');
-  const [textColor, setTextColor] = useState('white');
+  const [banner, setBanner] = useState('')
+  const [nav, setNav] = useState(false)
+  const [color, setColor] = useState('transparent')
+  const [textColor, setTextColor] = useState('white')
+
+  function showBanner() {
+    setBanner(true)
+  }
+
+  const AddBanner = {
+    text:
+      'Hajdeni kaloni pushimet te ne nga 1 Qershori – deri me 30 gusht 2023',
+    description: 'Hotel Toni',
+  }
 
   const handleNav = () => {
-    setNav(!nav);
-  };
+    setNav(!nav)
+  }
 
   useEffect(() => {
     const changeColor = () => {
       if (window.scrollY >= 90) {
-        setColor('#ffffff');
-        setTextColor('#000000');
+        setColor('#ffffff')
+        setTextColor('#000000')
       } else {
-        setColor('transparent');
-        setTextColor('#ffffff');
+        setColor('transparent')
+        setTextColor('#ffffff')
       }
-    };
-    window.addEventListener('scroll', changeColor);
-  }, []);
+    }
+    window.addEventListener('scroll', changeColor)
+  }, [])
 
   return (
-    <div
-      style={{ backgroundColor: `${color}` }}
-      className='fixed left-0 top-0 w-full z-10 ease-in duration-300'
-    >
-      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
-        <Link href='/'>
-          <h1 style={{ color: `${textColor}` }} className='font-bold text-4xl'>
-          {/* Hotel TONI */}
-          </h1>
-        </Link>
-        <ul style={{ color: `${textColor}` }} className='hidden sm:flex'>
-          <li className='p-4'>
-            <Link href='/'>Rreth Nesh</Link>
-          </li> 
-          <li className='p-4'>
-            <Link href='/rooms'>Akomodimi</Link>
-          </li>
-          {/* <li className='p-4'>
-            <Link href='/work'>Restorant</Link>
-          </li> */}
-          {/* <li className='p-4'>
-            <Link href='/aktivitete'>🇬🇧</Link>
-          </li> */}
-         
-          <li className='p-4'>
-            <Link href='/contact'>Kontakt</Link>
-          </li>
-          <li className='p-4'>
-            <Link href='/votat'>Votat ne bookning</Link>
-          </li>
-          <li className='p-4'>
-            <Link href='/aktivitete'>🇬🇧</Link>
-          </li>
-        </ul>
+    <>
+      <div
+        style={{ backgroundColor: `${color}` }}
+        className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+      >
+        <Banner {...AddBanner} />
+        <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+          <Link href="/">
+            <h1
+              style={{ color: `${textColor}` }}
+              className="font-bold text-4xl"
+            >
+              {/* Hotel TONI */}
+            </h1>
+          </Link>
+          <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
+            <li className="p-4">
+              <Link href="/">Rreth Nesh</Link>
+            </li>
+            <li className="p-4">
+              <Link href="/rooms">Akomodimi</Link>
+            </li>
 
-        {/* Mobile Button */}
-        <div onClick={handleNav} className='block sm:hidden z-10'>
-          {nav ? (
-            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
-          ) : (
-            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
-          )}
-        </div>
-        {/* Mobile Menu */}
-        <div
-          className={
-            nav
-              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
-              : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
-          }
-        >
-          <ul>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/'>Rreth Nesh</Link>
+            <li className="p-4">
+              <Link href="/contact">Kontakt</Link>
             </li>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/rooms'>Akomodimi</Link>
-            </li> 
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/work'>Restorante</Link>
+            <li className="p-4">
+              <Link href="/votat">Votat ne bookning</Link>
             </li>
-            {/* <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/aktivitete'>Aktivitete</Link>
-            </li> */}
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/contact'>Kontakt</Link>
-            </li>
-            <li className='p-4'>
-            <Link href='/votat'>Votat ne bookning</Link>
-          </li>
-          <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/work'>AL</Link>
+            <li className="p-4">
+              <Link href="/aktivitete">🇬🇧</Link>
             </li>
           </ul>
+
+          {/* Mobile Button */}
+          <div onClick={handleNav} className="block sm:hidden z-10">
+            {nav ? (
+              <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+            ) : (
+              <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
+            )}
+          </div>
+          {/* Mobile Menu */}
+          <div
+            className={
+              nav
+                ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+                : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+            }
+          >
+            <ul>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/">Rreth Nesh</Link>
+              </li>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/rooms">Akomodimi</Link>
+              </li>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/work">Restorante</Link>
+              </li>
+              {/* <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
+              <Link href='/aktivitete'>Aktivitete</Link>
+            </li> */}
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/contact">Kontakt</Link>
+              </li>
+              <li className="p-4">
+                <Link href="/votat">Votat ne bookning</Link>
+              </li>
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+              >
+                <Link href="/work">AL</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default Navbar;
-
-
+export default Navbar
