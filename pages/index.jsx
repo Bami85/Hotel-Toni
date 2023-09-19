ff5ea062830eb77240c7a76bca423469d9b2ebf3
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-09-19 21:46:33 +0200

    changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,12 +4,20 @@ import Hero from '../components/Hero'
 // import Instagram from '../components/Offers';
 import Footer from '../components/Footer'
 import Comments from '../components/Comments'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 
-export default function Home({text}) {
+
+
+
+export default function Home({ text }) {
+  const router = useRouter()
+  const id = router.query.id
   return (
     <>
       <div>
+     
         <Head>
           <title>Hotel Toni Llogora</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -19,15 +27,21 @@ export default function Home({text}) {
         <Hero
           heading="MIRËSEVINI TE HOTEL TONI "
           message="Lart mbi det.. Poshte nen qiell.  "
-          description ="Jeni i ftuar të përjetoni dhe të shijoni momente të pazakonta në mes te natyres, aty ku mysafirët ndihen si në shtëpi në dhomat ne mes te pishave te Llogorase."
-          
+          description="Jeni i ftuar të përjetoni dhe të shijoni momente të pazakonta në mes te natyres, aty ku mysafirët ndihen si në shtëpi në dhomat ne mes te pishave te Llogorase."
         />
 
-          <Comments>{text} </Comments>
-      <Footer/>
+        <Comments>
+        <h1>Dhoma: {id}</h1>
+          <ul>
+            <li>
+              <Link href={`/post/${id}/first-comment`}>First comment</Link>
+            </li>
+      </ul>
+
+      
+        </Comments>
+        <Footer />
       </div>
-   
     </>
   )
 }
-
