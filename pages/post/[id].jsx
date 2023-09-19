837679010321e8acc9b8a0609ad59610b8fb32ce
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-09-19 21:46:01 +0200

    DYNAMIC ROUTES

---
+++ pages/post/[id].jsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import Navbar from '../../components/Navbar'
+
+
+
+export default function RoomPage() {
+    const router = useRouter()
+    const id = router.query.id 
+  return (
+    <div>
+      <h1>Dhoma: {id}</h1>
+    </div>
+  )
+}
