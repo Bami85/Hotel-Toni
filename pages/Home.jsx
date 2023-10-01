13282c571173ed850bce869e89bc6f433399dbb6
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-02 01:34:26 +0200

    changes

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,8 +2,14 @@ import Head from 'next/head';
 import Image from 'next/image';
 // import Hero from '../components/Hero';
 // import MapComponent from '../components/MapComponent';
+import { useTranslation } from 'react-i18next';
 
 export default function Home() {
+  const { t } = useTranslation();
+  //change languages function
+  const handleLanguageChange = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   return (
     <div>
       <Head>
@@ -14,9 +20,12 @@ export default function Home() {
       
       <div className='relative'>
         <video autoPlay loop muted className='w-full h-auto'>
-          <source src='/llogoravideo.mp4' type='video/mp4' /> {/* Replace with the path to your video file */}
+          <source src='/llogoravideo.mp4' type='video/mp4' /> 
         </video>
-        <div className='absolute inset-0 bg-black/70 z-10'></div>
+        <div className='absolute inset-0 bg-black/70 z-10'>
+        <h1>{t('welcome')}</h1>
+        <p>{t('description')}</p>
+        </div>
       </div>
     </div>
   );
