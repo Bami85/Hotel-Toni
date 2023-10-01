a657f560ffe2dfd90faa8fced5d654f2769f43e2
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-02 01:33:46 +0200

    changes

--- interfaces/index.jsx
+++ interfaces/index.jsx
@@ -1,12 +1,11 @@
 
 const Person = {
-  name: '',
-  height: '',
-  mass: '',
-  hair_color: '',
-  skin_color: '',
-  eye_color: '',
-  gender: '',
+  guestName: '',
+  checkInDate: '',
+  checkOutDate: '',
+  price: '',
+  imageUrl: '',
+  description: '',
 };
 
 
