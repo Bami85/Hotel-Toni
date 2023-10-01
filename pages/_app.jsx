4ca6e0c8179ebebf12c423eee8e89ee7c4e865cc
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-02 01:34:21 +0200

    changes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './ i18n';
+
 
 function MyApp({ Component, pageProps }) {
  
@@ -12,6 +15,8 @@ function MyApp({ Component, pageProps }) {
   
       <Navbar />
       <Component {...pageProps} />
+      <I18nextProvider i18n={i18n}/>
+  
     </>
   );
 }
