4aa58ddd4bb5479f927743cea5b2f0c5271e1aff
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-04 11:25:54 +0200

    google tag manager

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,46 +1,50 @@
-
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-    return (
-      <Html>
-        <Head>
-          {/* Preconnect to Google Analytics */}
-          <link rel='preconnect' href='https://www.googletagmanager.com' />
+  return (
+    <Html>
+      <Head>
+        {/* Preconnect to Google Analytics */}
+        <link rel='preconnect' href='https://www.googletagmanager.com' />
 
-          {/* Google Analytics Script */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-0WWR22P54L"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-0WWR22P54L');
-              `,
-            }}
-          />
+        {/* Google Tag Manager Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PSDV6F6L');
+            `,
+          }}
+        />
 
-          {/* Your other head elements */}
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link
-            rel='preconnect'
-            href='https://fonts.gstatic.com'
-            crossOrigin='true'
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+        {/* Your other head elements */}
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PSDV6F6L"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
 
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
