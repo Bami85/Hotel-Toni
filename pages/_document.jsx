26e40cb306406a82d1ecbc1e7b61a739d6155296
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-04 10:50:37 +0200

    add google analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,24 +1,46 @@
+
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+    return (
+      <Html>
+        <Head>
+          {/* Preconnect to Google Analytics */}
+          <link rel='preconnect' href='https://www.googletagmanager.com' />
+
+          {/* Google Analytics Script */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-0WWR22P54L"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-0WWR22P54L');
+              `,
+            }}
+          />
+
+          {/* Your other head elements */}
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+
