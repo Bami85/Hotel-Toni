e0c13b6eb14f0d7a7a33030bda8ccf43f7a0277d
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-19 23:28:50 +0200

    remove google analytics

--- pages/reservationForm.jsx
+++ pages/reservationForm.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import ReservationForm from '../components/ReservationForm'
 import Offers from '../components/Offers'
 import Hero from '../components/Hero'
+import Link from 'next/link'
 
 const reservationForm = ({ selectedRoomId, handleReservationConfirmed }) => {
   return (
     <div>
       {/* <ReservationForm  heading='REZERVO TANI' Offers = {Offers}/> */}
-
+      <Link href ="/reservationForm">
       <Offers
+     
         roomId={selectedRoomId}
         onReservationConfirmed={handleReservationConfirmed}
         ReservationForm={ReservationForm}
       />
+      </Link>
     </div>
   )
 }
