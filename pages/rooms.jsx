9343d9f2b64fdfc0641119ce155912eafd743e73
Bami85 <91563447+Bami85@users.noreply.github.com>
2023-10-19 11:46:35 +0200

    rooms components

--- pages/rooms.jsx
+++ pages/rooms.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Offers from '../components/Offers'
-import Footer from '../components/Footer'
+// import React from 'react'
+// import Hero from '../components/Hero'
+// import Offers from '../components/Offers'
+// import Footer from '../components/Footer'
 
-const Rooms = () => {
-  return (
-    <div>
-        <Hero heading='Rooms comming soon' message='Kto jan disa nga dhomat qe mund te reservoni ne Hotell Toni' />
+// const Rooms = () => {
+//   return (
+//     <div>
+//         <Hero heading='Rooms comming soon' message='Kto jan disa nga dhomat qe mund te reservoni ne Hotell Toni' />
          
-        <Offers/>
-        <Footer/>
+//         <Offers/>
+//         <Footer/>
 
       
+//     </div>
+//   )
+// }
+
+// export default Rooms
+
+
+import React from 'react'
+
+export default function Rooms() {
+  return (
+    <div>
+      <h1>show rooms</h1>
     </div>
   )
 }
-
-export default Rooms
